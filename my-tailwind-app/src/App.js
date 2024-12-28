80d8ca60b9994fcbd983ae9ffdcb328c5019fa69
KAGAWADPARTZ <inocenciodaniel13@gmail.com>
2024-12-28 22:27:39 +0800

    try3

--- my-tailwind-app/src/App.js
+++ my-tailwind-app/src/App.js
@@ -9,9 +9,7 @@ function App() {
     <div className="w-full h-screen flex justify-center items-center bg-pink-300">
  
         <title>Dynamic Title</title>
-       
-    
-      <div className="h-1/2 w-1/3 bg-white p-5 rounded-lg shadow-lg flex justify-center items-center flex-col border-4 border-pink-700 text-center gap-4">
+      <div className="h-1/2 w-1/2 bg-white p-5 rounded-lg shadow-lg flex justify-center items-center flex-col border-4 border-pink-700 text-center gap-4">
         <h1 className="text-3xl font-bold text-rose-700 tracking-tight">
           Welcome to Tailwind  
         </h1>
