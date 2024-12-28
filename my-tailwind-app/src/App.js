4d903b83f65208aa35ce4b43b4d90983765bcb23
KAGAWADPARTZ <inocenciodaniel13@gmail.com>
2024-12-28 22:20:47 +0800

    try2

--- my-tailwind-app/src/App.js
+++ my-tailwind-app/src/App.js
@@ -6,15 +6,19 @@ function App() {
   const name ='Danielito';
   return (
     
-    <div className="w-full h-screen flex justify-center items-center bg-slate-600">
+    <div className="w-full h-screen flex justify-center items-center bg-pink-300">
  
         <title>Dynamic Title</title>
        
     
-      <div className="h-1/2 w-1/4 bg-white p-5 rounded-lg shadow-lg flex justify-center items-center flex-col">
-        <h1 className="text-3xl font-bold text-red-600 ">
-          Welcome to Tailwind CSS with React! {name}
+      <div className="h-1/2 w-1/3 bg-white p-5 rounded-lg shadow-lg flex justify-center items-center flex-col border-4 border-pink-700 text-center gap-4">
+        <h1 className="text-3xl font-bold text-rose-700 tracking-tight">
+          Welcome to Tailwind  
         </h1>
+        <h1 className="text-3xl font-bold text-rose-700 tracking-tight">
+           CSS with React! 
+        </h1>
+        <p className="text-3xl font-bold text-gray-800 tracking-tight">{name}</p>
       </div>
     </div>
   );
